432628048f9b53b1e52c182f8cd170e3febea037
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-04-03 19:02:48 +0200

    finished

--- BackendReview.API/obj/Debug/net8.0/BackendReview.API.AssemblyInfo.cs
+++ BackendReview.API/obj/Debug/net8.0/BackendReview.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendReview.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf55a50ccf5c63fa60614c912e366207bb9a859e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bf58fa1c062a0f261f81075f40a18ea8703e227")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendReview.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendReview.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
